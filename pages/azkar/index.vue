50893255ab1ab456bbe89427fda96034e282231e
Abdelrahman Hany <98924552+AbdelrhmanAbuelnile@users.noreply.github.com>
2023-08-22 11:34:43 +0300

    fixed zekr page

--- pages/azkar/index.vue
+++ pages/azkar/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w-full bg-mushafBg flex flex-col flex-1 items-center justify-center gap-4 p-4">
 
-      <NuxtLink :to="`azkar/${z.id}`" v-for="z in zekr" :key="z.id" class="card text-center w-full flex flex-row-reverse justify-between items-center px-4 lg:px-16 text-slate-200 bg-Dark-Grayish-Blue hover:bg-[#545c6b] hover:text-[#23cfa1] duration-300 ">
+      <NuxtLink :to="`azkar/${z.id-1}`" v-for="z in zekr" :key="z.id" class="card text-center w-full flex flex-row-reverse justify-between items-center px-4 lg:px-16 text-slate-200 bg-Dark-Grayish-Blue hover:bg-[#545c6b] hover:text-[#23cfa1] duration-300 ">
         <p class="font-bold text-center">{{ z.category }}</p>
       </NuxtLink>
 
