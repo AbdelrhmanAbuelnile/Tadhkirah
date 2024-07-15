5719ca81b72d56f589fe344c2b5da89f594f4b2c
Abdelrahman Hany <98924552+AbdelrhmanAbuelnile@users.noreply.github.com>
2024-07-15 06:22:37 +0300

    fixed ui issues and basic logic

--- components/RandomAyah.vue
+++ components/RandomAyah.vue
@@ -1,47 +1,48 @@
 <template>
-    <div class="flex flex-col justify-center items-center w-11/12 lg:w-2/4 h-fit bg-Dark-Grayish-Blue rounded-2xl text-center gap-6 py-14 px-4 relative">
-        <h1 class="text-red-500 text-3xl font-bold">{{ ayahNo }} , {{ surahName }}</h1>
+    <div
+        class="flex flex-col justify-center items-center w-11/12 lg:w-2/4 h-fit bg-Dark-Grayish-Blue rounded-2xl text-center gap-6 py-14 px-4 relative">
+        <h1 class="text-Neon-Green text-3xl font-bold">
+            {{ surahName }} , {{ ayahNo }}
+        </h1>
         <p class="text-slate-200 text-3xl font-semibold">{{ ayah }}</p>
-
-        <button
-        @click="fetchAyah"
-        class="p-2 bg-cyan-500  font-bold text-lg hover:bg-Light-Cyan duration-200 rounded-full"
-        >
-            <img src="../assets/imgs/refresh.png" alt="new ayah button" class="w-6">
+        <button @click="fetchAyah"
+            class="p-2 bg-Neon-Green font-bold text-lg hover:bg-Light-Cyan duration-200 rounded-full">
+            <img src="../assets/imgs/refresh.png" alt="new ayah button" class="w-6" />
         </button>
     </div>
 </template>
 
 <script setup>
-    import { onMounted, ref } from 'vue';
-    import quran from '../data/quran.json'
+import { ref } from 'vue';
+import quran from '../data/quran.json';
 
-    const surahName = ref('')
-    const ayahNo = ref(null)
-    const ayah = ref('')
-    const ayatNo = ref(null)
-    
-    
-    
-    
-    const fetchAyah = () => {
-        const randomSurah = Math.floor(Math.random() * 114) + 1;
-        let surah = quran.data.surahs[randomSurah]
-        
-        surahName.value = surah.name
-        ayatNo.value = surah.ayahs.length
-        const randomNumberOfAyah = Math.floor(Math.random() * ayatNo.value) + 1 ;
-        ayah.value = surah.ayahs[randomNumberOfAyah].text
+const surahName = ref("ٱلْفَاتِحَةِ");
+const ayahNo = ref("١");
+const ayah = ref("بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ");
 
-        ayahNo.value = surah.ayahs[randomNumberOfAyah].numberInSurah
-    }
-    fetchAyah()
+const fetchAyah = () => {
+    setTimeout(() => {
+        const randomSurahIndex = Math.floor(Math.random() * 114); // Adjusted to zero-based index
+        const surah = quran.data.surahs[randomSurahIndex];
+        const ayatNo = surah.ayahs.length;
+        const randomAyahIndex = Math.floor(Math.random() * ayatNo); // Adjusted to zero-based index
+        surahName.value = surah.name;
+        ayah.value = surah.ayahs[randomAyahIndex].text;
+        ayahNo.value = convertToArabic(surah.ayahs[randomAyahIndex].numberInSurah);
+    }, 100);
+};
+
+function convertToArabic(number) {
+    const arabicNumbers = ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'];
+    return number.toString().split('').map(digit => arabicNumbers[parseInt(digit, 10)]).join('');
+}
+
 </script>
 
 <style scoped>
-    button {
-        position: absolute;
-        bottom: 0;
-        transform: translateY(50%);
-    }
+button {
+    position: absolute;
+    bottom: 0;
+    transform: translateY(50%);
+}
 </style>
--- error.vue
+++ error.vue
@@ -1,20 +1,34 @@
 <template>
-  <div class="bg-green-500 w-full h-screen flex flex-col justify-center items-center">
+  <div class="bg-mushafBg text-slate-600 w-full h-screen flex flex-col justify-center items-center">
     <div class="h-2/4 w-3/4 md:w-2/4 md:h-2/6 lg:w-1/4 lg:h-2/4 mx-auto text-center card">
-        <p class=" mt-7 text-7xl font-bold">{{ error.statusCode }}</p>
-        <p class="mt-7 text-6xl ">Ooops.</p>
-        <p class=" mt-7">{{ error.message }}</p>
-        <button class="btn my-7" @click="handleClearError">Go Home...</button>
+      <p class=" mt-7 text-7xl font-bold">{{ error.statusCode }}</p>
+      <p class="mt-7 text-6xl ">Ooops.</p>
+      <p class=" mt-7">{{ error.message }}</p>
+      <button class="btn my-7" @click="handleClearError">Go Home...</button>
     </div>
   </div>
 </template>
 
 <script setup>
-  defineProps(['error']) 
+defineProps(['error'])
 
-  const handleClearError = () => clearError({ redirect: '/'})
+const handleClearError = () => clearError({ redirect: '/' })
+
+useSeoMeta({
+  title: "Oops!",
+  description:
+    "موقع تذكرة - تطبيق ويب شامل يحتوي على القرآن الكريم، الحديث الشريف، التفسير، والأذكار في مكان واحد.",
+  ogTitle: "تذكرة - مرجعك للموارد الإسلامية",
+  ogDescription:
+    "استمتع بسهولة الوصول إلى القرآن الكريم، الحديث الشريف، التفسير، والأذكار عبر تطبيق ويب تذكرة.",
+  ogImage: "/favicon.ico",
+  ogUrl: "https://tadhkirah.vercel.app/",
+  twitterTitle: "تذكرة - مصدرك للموارد الإسلامية",
+  twitterDescription:
+    "اكتشف متعة الوصول السهل إلى القرآن الكريم، الحديث الشريف، التفسير، والأذكار عبر تطبيق ويب تذكرة.",
+  twitterImage: "/favicon.ico",
+})
+
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
